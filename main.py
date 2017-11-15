6c2607d231602626227534228bea42d128659057
Denys Vitali <denys.vitali@coelis.ch>
2017-11-15 15:06:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from klein import run, route
+from subprocess import call
+
+@route('/')
+def home(request):
+    return open("./index.html", 'r').read()
+
+@route('/next')
+def next(req):
+    call(["zsh", '-c' , 'xdotool key --window "$(xdotool search "Slides" | head -n1)" Right'])
+    return "ok"
+
+
+@route('/prev')
+def prev(req):
+    print("Got prev")
+    call(["zsh", '-c' , 'xdotool key --window "$(xdotool search "Slides" | head -n1)" Left'])
+    return "ok"
+
+
+run("192.168.43.34", 8080)
